dd6e1e1bd5df7295e56bf7f5aa3af356904f709c
irenemg2 <irenemg2@TR12-S03-30>
2026-09-21 11:45:08 +0200

    version 1

--- Repo.c
+++ Repo.c
@@ -1,2 +1,19 @@
-#include
-//jjijojo
+#include<stdio.h>
+
+//suma fecha cumple
+
+int main() {
+	int dia, mes, año;
+
+	printf("Escribe tu día de nacimiento (2 numeros)");
+	scanf_s("%d", &dia);
+
+	printf("Escribe tu mes de nacimiento (2 numeros)");
+	scanf_s("%d", &mes);
+
+	printf("Escribe tu año de nacimiento (4 numeros)");
+	scanf_s("%d", &año);
+
+	printf("La suma de tu fecha de nacimiento es:");
+	printf("%d + %d = %d", dia, mes, año);
+}
